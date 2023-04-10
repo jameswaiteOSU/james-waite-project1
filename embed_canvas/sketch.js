cfad7ef438302f2a8d4d73995fc4e7ec1711cca6
jameswaiteOSU <waite.148@buckeyemail.osu.edu>
2023-04-10 09:21:22 -0400

    embed canvas example

---
+++ embed_canvas/sketch.js
@@ -0,0 +1,22 @@
+const words = ["brown", "fox", "jumped", "quickly"]
+const numbers = [18, 36, 24, 12];
+let index = 0;
+
+function setup() {
+    myCanvas = createCanvas(600, 400);
+    myCanvas.parent('myContainer');
+
+    setInterval(changeInterval, 1000);
+}
+
+function draw() {
+    background(150);
+    textAlign(CENTER, CENTER);
+    textSize(numbers[index])
+    text(words[index], width/2, height/2);
+}
+
+function changeInterval() {
+    index++;
+    (index >= words.length) ? index = 0 : null;
+}
